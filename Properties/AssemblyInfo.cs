358c2af23825e033f12a4b15e3a50b3e980d97f5
Namdak Tonpa <maxim@synrc.com>
2021-08-07 04:09:35 +0300

    MIA-AGENT rename

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MIA-SCAN")]
-[assembly: AssemblyDescription("MIA: Scanning Windows Tray Application")]
+[assembly: AssemblyTitle("MIA-AGENT")]
+[assembly: AssemblyDescription("MIA: Windows Tray Companion Application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("INFOTECH SE")]
 [assembly: AssemblyProduct("MIA: Scanning")]
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("95c1ce5e-1b22-4739-a390-30ddda10f984")]
-[assembly: AssemblyVersion("2.4.11.0")]
-[assembly: AssemblyFileVersion("2.4.11.0")]
+[assembly: AssemblyVersion("2.8.2.0")]
+[assembly: AssemblyFileVersion("2.8.2.0")]
