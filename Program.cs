fd22c9e8b89b8e07c0eb28bc89506855590c283c
Namdak Tonpa <maxim@synrc.com>
2021-08-07 19:56:43 +0300

    cleanup trash comments

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace INFOTECH
@@ -11,10 +8,9 @@ namespace INFOTECH
         [STAThread]
         static void Main()
         {
-            N2O.Server.start("0.0.0.0", 1900);
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            N2O.Server.start("0.0.0.0",1900);
             Application.Run(new FormScan());
             Application.Exit();
         }
