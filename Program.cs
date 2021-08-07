306fc7ee4961a43702b6ce4b9151d99c3788909c
Namdak Tonpa <maxim@synrc.com>
2021-08-07 16:14:29 +0300

    Embedded web-socket server

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace INFOTECH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            N2O.Server.start("0.0.0.0", 1900);
             Application.Run(new FormScan());
             Application.Exit();
         }
