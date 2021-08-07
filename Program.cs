089c14da011c244d68b39979d253a87052e6892d
Namdak Tonpa <maxim@synrc.com>
2021-08-07 19:57:03 +0300

    cleanup trash comments

--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@ namespace INFOTECH
         [STAThread]
         static void Main()
         {
+            N2O.Server.start("0.0.0.0",1900);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            N2O.Server.start("0.0.0.0",1900);
             Application.Run(new FormScan());
             Application.Exit();
         }
